9482c5c89429cd56b2df5b0ae06f79208420e6a5
Avithegitty81 <64101146+Avithegitty81@users.noreply.github.com>
2020-08-19 06:13:59 -0500

    HW done

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Javascript-Challenge
 Javascript HW
+
+In this challenge we have write the javascript fo the UFO-Sighting webpage just so we could create an interractive dashboard to filter and search various UFO sightings across the globe.
---
+++ UFO-Level-1/StarterCode/static/js/app.js
@@ -0,0 +1,64 @@
+// Use data from data.js
+var tableData = data;
+var table = d3.select("tbody")
+
+// Pull and display all data
+tableData.forEach((ufo) => {
+    var row = table.append("tr");
+    Object.values(ufo).forEach(value => {
+        var cell  = row.append  ("td");
+        cell.text(value);
+
+    });
+
+});
+
+//  Select the button
+var button = d3.select("#button");
+
+// Select the form
+var form_date = d3.select("#form-date");
+
+// Create the event handler
+button.on("click", runEnter);
+form_date.on("submit", runEnter);
+
+//Complete the event handler function for the form
+function runEnter() {
+    //Clear table:
+    table.html("");
+
+//Prevent the page from refreshing
+d3.event.preventDefault();
+
+// Get the value property of the input elements
+date = d3.select("#UFO-form-date").property("value");
+
+//Filter based on the user filter
+function date_filter(ufo){
+    if (date!=""){
+        return ufo.datetime === date;
+    }else{
+        return true;
+    }
+}
+filtered_data = tableData.filter(date_filter);
+
+filtered-data.forEach((ufo) => {
+    var row = table.append("tr");
+    Object.value(ufo).forEach(value => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+
+};
+
+
+
+
+
+
+
+
